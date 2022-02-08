55a09fa5bb8d94d24fe0026a810574531069c64a
Imman Hussain <iah29@cam.ac.uk>
2022-02-08 10:09:52 +0000

    jnjjnhnhnjh

--- test_geo.py
+++ test_geo.py
@@ -14,11 +14,11 @@ def test_stations_within_radius():
 def test_rivers_by_station_number():
     stations = build_station_list()
     req_list = rivers_by_station_number(stations, N=9)
-    print(req_list)
     #check that number of stations are in descending order
     for i in range(len(req_list)-1):
         assert req_list[i][1] >= req_list[i+1][1]
         #check that all river names are strings
         assert isinstance(req_list[i][0], str)
 
+test_stations_within_radius()
 test_rivers_by_station_number()
