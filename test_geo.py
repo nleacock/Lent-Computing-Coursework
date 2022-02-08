bdeb14ec11fa879b8af988b9dfabd668af3a09f2
Imman Hussain <iah29@cam.ac.uk>
2022-02-08 09:14:14 +0000

    test for stations_within_radius works?(sort of)

--- test_geo.py
+++ test_geo.py
@@ -5,10 +5,11 @@ from haversine import haversine, Unit
 
 def test_stations_within_radius():
     stations = build_station_list()
-    within_radius = stations_within_radius(stations)
+    within_radius = stations_within_radius(stations, (52.2053, 0.1218), 10)
     #check that each entry is a string
     for station in within_radius:
         assert isinstance(station, str)
+    
 
-
-
+def test_rivers_by_station_number():
+    stations = build_station_list()
